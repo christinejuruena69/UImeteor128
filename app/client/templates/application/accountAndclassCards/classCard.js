21fc2fa6744baea5b4562da794e49d3eba644696
christinejuruena69 <christinemaejuruena@gmail.com>
2016-03-15 14:42:25 +0800

    Dummy data added, edited the view teacher account. syntaxes are still flawed. to be edited later.

---
+++ app/client/templates/application/accountAndclassCards/classCard.js
@@ -0,0 +1,22 @@
+// var courseData = [
+// 	{
+// 		courseCode: 'CMSC128',
+// 		courseTitle: 'Introduction to Software Engineering'
+// 	},
+// 	{
+// 		courseCode: 'CMSC132',
+// 		courseTitle: 'cOMPUTER aRCHTECTURE'
+// 	},
+// 	{
+// 		courseCode: 'CMSC170',
+// 		courseTitle: 'Artifcial intelligence'
+// 	},
+// 	{
+// 		courseCode: 'CMSC125',
+// 		courseTitle: 'operating system'
+// 	}
+// ];
+
+// Template.classCard.helpers({
+// 	courses: courseData
+// });
---
+++ app/client/templates/application/viewTeacherAccount/viewTeacherAccount.js
@@ -0,0 +1,29 @@
+
+/*****************************************************************************/
+/* Home: Event Handlers */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Home: Helpers */
+/*****************************************************************************/
+Template.viewTeacherAccount.helpers({
+	loadCourses : function(){
+		Session.set('adminAccount', false);
+        Session.set('teacherAccount', true);
+        return true;
+	}
+});
+
+
+
+/*****************************************************************************/
+/* Home: Lifecycle Hooks */
+/*****************************************************************************/
+Template.viewTeacherAccount.onCreated(function () {
+});
+
+Template.viewTeacherAccount.onRendered(function () {
+});
+
+Template.viewTeacherAccount.onDestroyed(function () {
+});
