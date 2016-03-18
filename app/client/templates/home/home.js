a01c6af7b09581c6056df1d68f8e6ed126206f71
christinejuruena69 <christinemaejuruena@gmail.com>
2016-03-18 10:35:06 +0800

    edited syntax

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -5,39 +5,23 @@
 
 // });
 
-// Session.setDefault('myTemplate', 'Home');
-// "click .adminbtn": function (event) {
-//     Session.set("myTemplate", 'accountCard');
-// }
-
-// good
-// Session.setDefault('accountCard', false);
-// Template.body.isTrue = function() { Session.get('accountCard'); }
-// Template.Home.events({
-//     'click .adminbtn': function(){
-//         //load a new template
-//         console.log("You pressed the addNewplace button");//this fn is called properly
-//            Session.set('accountCard', true);
-//            Blaze.renderWithData(Template.accountCard);
-//     }
-// });
 Template.Home.events({
-    'click .adminbtn': function(){
-        console.log("Pressed");//this fn is called properly
+    'click .adminbtn': function() {
+        console.log("Pressed"); //this fn is called properly
         Session.set('teacherAccount', false);
         Session.set('adminAccount', true);
     },
-    'click .teacherbtn': function(){
-        console.log("Pressed teachr");//this fn is called properly
+    'click .teacherbtn': function() {
+        console.log("Pressed teachr"); //this fn is called properly
         Session.set('adminAccount', false);
         Session.set('teacherAccount', true);
     }
 });
-Template.Home.helpers({    
-    'adminAccount': function(){
+Template.Home.helpers({
+    'adminAccount': function() {
         return Session.get('adminAccount');
     },
-    'teacherAccount': function(){
+    'teacherAccount': function() {
         return Session.get('teacherAccount');
     }
 
@@ -47,25 +31,23 @@ Template.Home.helpers({
 /* Home: Helpers */
 /*****************************************************************************/
 // Template.Home.helpers({
-// 	// tab: function() {
+//  // tab: function() {
 //  //    return Template.instance().currentTab.get();
 //  //  },
-  
+
 // });
 
 /*****************************************************************************/
 /* Home: Lifecycle Hooks */
 /*****************************************************************************/
-Template.Home.onCreated(function () {
-	 // this.UserTemplate = new ReactiveVar( false );
-	 // this.currentTab = new ReactiveVar( "accountCard" );
+Template.Home.onCreated(function() {
+    // this.UserTemplate = new ReactiveVar( false );
+    // this.currentTab = new ReactiveVar( "accountCard" );
 });
 
-Template.Home.onRendered(function () {
-});
+Template.Home.onRendered(function() {});
 
-Template.Home.onDestroyed(function () {
-});
+Template.Home.onDestroyed(function() {});
 
 
 
